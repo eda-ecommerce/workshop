5969c3ed904bfbbd688021e5a8b5999fb8deb4e6
JannikAlx <jannik-alx@gmx.de>
2024-11-23 14:45:43 +0100

    feat: complete shippingController

--- exercise5/src/main/java/com/eda/shippingService/adapters/web/ShippingController.java
+++ exercise5/src/main/java/com/eda/shippingService/adapters/web/ShippingController.java
@@ -1,15 +1,22 @@
 package com.eda.shippingService.adapters.web;
 
 import com.eda.shippingService.application.service.ShipmentService;
+import com.eda.shippingService.domain.dto.common.AddressDTO;
 import com.eda.shippingService.domain.dto.incoming.IncomingPackageDTO;
 import com.eda.shippingService.domain.dto.incoming.ShipmentContentsDTO;
 import com.eda.shippingService.domain.dto.incoming.UpdateShipmentStatusDTO;
 import com.eda.shippingService.domain.dto.outgoing.ShipmentDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -18,44 +25,95 @@ public class ShippingController {
     private final ShipmentService shipmentService;
 
     @Autowired
-    public ShippingController(
-            ShipmentService shipmentService
-    ) {
+    public ShippingController(ShipmentService shipmentService) {
         this.shipmentService = shipmentService;
     }
+    @Operation(summary = "Get all shipments")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "All shipments retrieved successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ShipmentDTO.class)) }),
+    })
+    @GetMapping("/")
+    public ResponseEntity<List<ShipmentDTO>> findAllShipments() {
+        return ResponseEntity.ok(shipmentService.findAllShipments());
+    }
 
-    @Operation(summary = "Request a shipment")
+    @Operation(summary = "Request a shipment manually")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Shipment requested successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ShipmentDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid input",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Order not found",
+                    content = @Content)
+    })
     @PostMapping("/{orderId}")
     public ResponseEntity<ShipmentDTO> requestShipment(@PathVariable UUID orderId, @RequestBody ShipmentContentsDTO shipmentDTO) {
         return ResponseEntity.ok(shipmentService.provideRequestedContents(orderId, shipmentDTO));
     }
 
+    @Operation(summary = "Set shipment destination")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Shipment destination set successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ShipmentDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid input",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Order not found",
+                    content = @Content)
+    })
+    @PostMapping("/{orderId}/destination")
+    public ResponseEntity<ShipmentDTO> setDestination(@RequestBody AddressDTO addressDTO, @PathVariable UUID orderId) {
+        return ResponseEntity.ok(shipmentService.provideShippingAddress(orderId, addressDTO));
+    }
+
+    @Operation(summary = "Box a shipment")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Shipment boxed successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ShipmentDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid input",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Order not found",
+                    content = @Content)
+    })
+    @PostMapping("/{orderId}/package")
+    public ResponseEntity<ShipmentDTO> boxShipment(@RequestBody IncomingPackageDTO packageDTO, @PathVariable UUID orderId) {
+        var dto = shipmentService.boxShipment(orderId, packageDTO);
+        return ResponseEntity.ok(dto);
+    }
+
+    @Operation(summary = "Update shipment status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Shipment status updated successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ShipmentDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid input",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Order not found",
+                    content = @Content)
+    })
     @PostMapping("/{orderId}/status")
     public ResponseEntity<ShipmentDTO> setStatus(@RequestBody UpdateShipmentStatusDTO shipmentDTO) {
         return ResponseEntity.ok(shipmentService.externalShipmentStatusUpdate(shipmentDTO));
     }
 
-    @PostMapping("/{orderId}/package")
-    public ResponseEntity<ShipmentDTO> boxShipment(@RequestBody IncomingPackageDTO packageDTO, @PathVariable UUID orderId){
-        var dto = shipmentService.boxShipment(orderId, packageDTO);
-        return ResponseEntity.ok(dto);
+    @Operation(summary = "Send a shipment on its way")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Shipment sent successfully",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ShipmentDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid input",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Order not found",
+                    content = @Content)
+    })
+    @PutMapping("/{orderId}/send")
+    public ResponseEntity<ShipmentDTO> sendShipment(@PathVariable UUID orderId) {
+        return ResponseEntity.ok(shipmentService.sendShipment(orderId));
     }
-
-    /*
-    Routes:
-    POST /shipment
-    GET /shipment/{orderId}
-    GET /shipments
-    DELETE /shipment/{orderId}
-
-    Event triggers:
-    - createShipment (Command)
-    - cancelShipment (Command)
-    - processProductDelivery (Command)
-    - shipmentDelivered (Event)
-    - orderCreated (Event) --> StockCheck --> Shipment Requested/Impossible
-    - orderConfirmed (Event) --> SendShipment()
-     */
 
 
 }
