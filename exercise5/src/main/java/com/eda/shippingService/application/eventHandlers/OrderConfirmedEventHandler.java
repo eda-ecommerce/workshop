8828d247f2bd6b249c87f76769da8b145bbc1bc4
JannikAlx <jannik-alx@gmx.de>
2024-11-23 14:46:11 +0100

    chore: some logging

--- exercise5/src/main/java/com/eda/shippingService/application/eventHandlers/OrderConfirmedEventHandler.java
+++ exercise5/src/main/java/com/eda/shippingService/application/eventHandlers/OrderConfirmedEventHandler.java
@@ -5,9 +5,11 @@ import com.eda.shippingService.domain.entity.ProcessedMessage;
 import com.eda.shippingService.domain.events.OrderConfirmed;
 import com.eda.shippingService.adapters.repo.IdempotentHandlerRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class OrderConfirmedEventHandler implements EventHandler<OrderConfirmed> {
     private final ShipmentService shipmentService;
@@ -28,6 +30,7 @@ public class OrderConfirmedEventHandler implements EventHandler<OrderConfirmed> 
         }
         var payload = event.getMessageValue();
         shipmentService.approveShipment(payload.orderId());
+        log.info("OrderConfirmedEvent with ID: {} handled", event.getMessageId());
         idempotentHandlerRepository.save(new ProcessedMessage(event.getMessageId(), this.getClass().getSimpleName()));
     }
 }
