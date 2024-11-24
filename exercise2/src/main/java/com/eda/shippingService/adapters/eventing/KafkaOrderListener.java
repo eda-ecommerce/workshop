c4a80077fe38d92a8cb483181ab43bfa6441c140
JannikAlx <jannik-alx@gmx.de>
2024-11-24 18:28:07 +0100

    chore: unused imports

--- exercise2/src/main/java/com/eda/shippingService/adapters/eventing/KafkaOrderListener.java
+++ exercise2/src/main/java/com/eda/shippingService/adapters/eventing/KafkaOrderListener.java
@@ -1,17 +1,9 @@
 package com.eda.shippingService.adapters.eventing;
 
-import com.eda.shippingService.adapters.repo.IdempotentHandlerRepository;
-import com.eda.shippingService.application.service.IdempotentcyService;
-import com.eda.shippingService.domain.entity.ProcessedMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @Component
 public class KafkaOrderListener {
