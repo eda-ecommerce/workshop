6c885d6d3245083f890a472d1f67cefdc7cfe605
Jannik Alexander <jannik.alexander@innovation-hub.de>
2024-11-26 19:55:16 +0100

    ex2: fix OrderConfirmed event

--- exercise2/src/main/java/com/eda/shippingService/domain/events/OrderConfirmed.java
+++ exercise2/src/main/java/com/eda/shippingService/domain/events/OrderConfirmed.java
@@ -1,12 +1,12 @@
 package com.eda.shippingService.domain.events;
 
-import com.eda.shippingService.domain.dto.incoming.OrderConfirmedDTO;
+import com.eda.shippingService.domain.dto.incoming.OrderDTO;
 import com.eda.shippingService.domain.events.common.DomainEvent;
 
 import java.util.UUID;
 
-public class OrderConfirmed extends DomainEvent<OrderConfirmedDTO> {
-    public OrderConfirmed(UUID eventKey, UUID messageId, long timestamp, OrderConfirmedDTO payload) {
+public class OrderConfirmed extends DomainEvent<OrderDTO> {
+    public OrderConfirmed(UUID eventKey, UUID messageId, long timestamp, OrderDTO payload) {
         super(messageId, eventKey, timestamp, payload);
     }
 }
